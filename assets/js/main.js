ff7de928a7d2a25ea0d00b0a00946006264e85a0
shanelly <shanelle.encomienda@gmail.com>
2019-02-28 13:58:42 +0800

    added pics on homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+/*
+	Snapshot by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
 
 (function($) {
 
@@ -37,7 +42,62 @@
 		// Scrolly.
 			$('.scrolly').scrolly();
 
-				
+		// Gallery.
+			$('.gallery').each(function() {
+
+				var	$gallery = $(this),
+					$content = $gallery.find('.content');
+
+				// Poptrox.
+					$content.poptrox({
+						usePopupCaption: true
+					});
+
+				// Tabs.
+					$gallery.each( function() {
+
+						var $this = $(this),
+							$tabs = $this.find('.tabs a'),
+							$media = $this.find('.media');
+
+						$tabs.on('click', function(e) {
+
+							var $this = $(this),
+								tag = $this.data('tag');
+
+							// Prevent default.
+							 	e.preventDefault();
+
+							// Remove active class from all tabs.
+								$tabs.removeClass('active');
+
+							// Reapply active class to current tab.
+								$this.addClass('active');
+
+							// Hide media that do not have the same class as the clicked tab.
+								$media
+									.fadeOut('fast')
+									.each(function() {
+
+										var $this = $(this);
+
+										if ($this.hasClass(tag))
+											$this
+												.fadeOut('fast')
+												.delay(200)
+												.queue(function(next) {
+													$this.fadeIn();
+													next();
+												});
+
+									});
+
+						});
+
+					});
+
+
+			});
 
 	});
 
